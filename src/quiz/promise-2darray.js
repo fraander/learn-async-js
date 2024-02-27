35069a8026e3373a9a384ebb1e94517637d9e06c
fraander <57777918+fraander@users.noreply.github.com>
2024-02-27 17:02:06 -0500

    following along

--- src/quiz/promise-2darray.js
+++ src/quiz/promise-2darray.js
@@ -1,35 +1,38 @@
 function sum2DArray(arr) {
-    return new Promise((resolve, reject) => {
-        console.log('Sum called ... ');
-        if(Array.isArray(arr)) {
-            setTimeout(() => {
-                let sum = 0;
-                for (let i = 0; i < arr.length; i++) {
-                    for (let j = 0; j < arr[i].length; j++) {
-                        sum += arr[i][j];
-                    }
-                }
-                console.log('resolving ... ');
-                resolve(sum);
-            }, 0);
+  return new Promise((resolve, reject) => {
+    console.log("Sum called ... ");
+    if (Array.isArray(arr)) {
+      setTimeout(() => {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+          for (let j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j];
+          }
         }
-        else {
-            console.log('rejecting ... ');
-            reject('BAD INPUT: Expected array as input');
-        }
-        console.log('returning from sum');
-    });
+        console.log("resolving ... ");
+        resolve(sum);
+      }, 0);
+    } else {
+      console.log("rejecting ... ");
+      reject("BAD INPUT: Expected array as input");
+    }
+    console.log("returning from sum");
+  });
 }
 
 // Example usage:
 const array2D = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
 
-const sumPromise1 = sum2DArray(array2D);
+const sumPromise1 = sum2DArray(array2D)
+  .then((r) => console.log(r))
+  .catch((e) => console.log(e));
 console.log(sumPromise1);
 
-const sumPromise2 = sum2DArray('array2D');
+const sumPromise2 = sum2DArray("array2d")
+  .then((r) => console.log(r))
+  .catch((e) => console.log(e));
 console.log(sumPromise2);
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,47 @@
+// sum of a row is function
+// return a promise
+// Promise.all
+
+function sumRow(row) {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(row.sum());
+    } catch {
+      reject("BAD INPUT");
+    }
+  });
+}
+
+function sum2DArray(arr) {
+  return new Promise((resolve, reject) => {
+    console.log("Sum called ... ");
+    if (Array.isArray(arr)) {
+      setTimeout(async () => {
+        let sum = 0;
+        let promises = [];
+        for (let i = 0; i < arr.length; i++) {
+          promises.push(
+            sumRow(arr[i])
+              .then((r) => (sum += r))
+              .catch((e) => reject(e))
+          );
+        }
+        console.log("resolving ... ");
+        await Promise.all(promises);
+        resolve(sum);
+      }, 0);
+    } else {
+      console.log("rejecting ... ");
+      reject("BAD INPUT: Expected array as input");
+    }
+    console.log("returning from sum");
+  }).then((r) => console.log(r));
+}
+
 const array2D = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
+
+console.log(sum2DArray(array2D));
